52d28744cfea94e7560d50026ad816743d1eccef
Sebas <psvitasebas@gmail.com>
2022-07-19 03:11:17 -0500

    auxilio

--- server.js
+++ server.js
@@ -43,7 +43,9 @@ require('./app/routes/doctor.route')(app)
 require('./app/routes/horarioDoctor.route')(app)
 
 // set port, listen for requests
-const PORT = 5432
+
+var PORT = process.env.PORT || 4000;
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`)
 })
