c4d35c485c99519c96f7dc0f12f68aa2093364ac
Sebas <psvitasebas@gmail.com>
2022-07-19 08:27:33 -0500

    arreglo error

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const app = express()
 
 var corsOptions = {
-  origin: [']http://localhost:3000','https://frontend-clinica-chevere.herokuapp.com']
+  origin: ['http://localhost:3000','http://frontend-clinica-chevere.herokuapp.com']
 }
 
 app.use(cors(corsOptions))
