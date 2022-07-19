9b327a1c26fff88ddcaa3481ad490b9f1c587e4d
Sebas <psvitasebas@gmail.com>
2022-07-19 02:53:40 -0500

    cambiopuerto

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "postgres",
-  DB: "postgres",
+  HOST: "ec2-54-87-179-4.compute-1.amazonaws.com",
+  USER: "lvdviifljkeqrc",
+  PASSWORD: "2b361b92065db0a38932c07c47958c905da47e6ad302f9b8ecd3c08a0bc01110",
+  DB: "d8hgi0c480ftj5",
   dialect: "postgres",
   pool: {
     max: 5,
